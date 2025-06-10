784fbb128e8b4622a3aa606953365af7a477179c
kurimiyumi <sayadan4anjing@gmail.com>
2025-06-10 23:57:40 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -23,12 +23,12 @@ async fn main(req: Request, env: Env, _: Context) -> Result<Response> {
         .map(|x| Uuid::parse_str(&x.to_string()).unwrap_or_default())?;
     let host = req.url()?.host().map(|x| x.to_string()).unwrap_or_default();
     let main_page_url = env.var("MAIN_PAGE_URL").map(|x|x.to_string()).unwrap();
-    // let sub_page_url = env.var("SUB_PAGE_URL").map(|x|x.to_string()).unwrap();
-    let config = Config { uuid, host: host.clone(), proxy_addr: host, proxy_port: 443, main_page_url };
-    // let config = Config { uuid, host: host.clone(), proxy_addr: host, proxy_port: 443, main_page_url, sub_page_url };
+    let sub_page_url = env.var("SUB_PAGE_URL").map(|x|x.to_string()).unwrap();
+    // let config = Config { uuid, host: host.clone(), proxy_addr: host, proxy_port: 443, main_page_url };
+    let config = Config { uuid, host: host.clone(), proxy_addr: host, proxy_port: 443, main_page_url, sub_page_url };
     Router::with_data(config)
         .on_async("/", fe)
-        // .on_async("/sub", sub)
+        .on_async("/sub", sub)
         .on("/link", link)
         .on_async("/:proxyip", tunnel)
         .run(req, env)
@@ -45,9 +45,9 @@ async fn fe(_: Request, cx: RouteContext<Config>) -> Result<Response> {
     get_response_from_url(cx.data.main_page_url).await
 }
 
-// async fn sub(_: Request, cx: RouteContext<Config>) -> Result<Response> {
-//     get_response_from_url(cx.data.sub_page_url).await
-// }
+async fn sub(_: Request, cx: RouteContext<Config>) -> Result<Response> {
+    get_response_from_url(cx.data.sub_page_url).await
+}
 
 
 async fn tunnel(req: Request, mut cx: RouteContext<Config>) -> Result<Response> {
